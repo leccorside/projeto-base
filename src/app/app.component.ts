95af29c6a8bf017cbf1b9bee57390fb29ed4149a
Johnathan <leccorside@gmail.com>
2025-03-12 12:13:14 -0300

    Criação do sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatDividerModule, 
+    MatToolbarModule, 
+    MatSidenavModule, 
+    MatListModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'projeto-base';
+  showFiller = false;
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, MatButtonModule],
+  bootstrap: []
+})
+export class AppModule {}
